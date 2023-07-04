959beca464a2066509de5e2b41c16c9651cc8a74
Ashe-png <ashish.khatakho11@gmail.com>
2023-07-04 11:19:49 +0545

    changed signin button

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { getProviders, signIn } from 'next-auth/react';
 import { useEffect, useState } from 'react';
+import Button from './Button';
 
 type Provider = {
   id: string;
@@ -30,9 +31,11 @@ const AuthProviders = () => {
     return (
       <div>
         {Object.values(providers).map((provider: Provider, index) => (
-          <button key={index} onClick={() => signIn(provider?.id)}>
-            {provider.id}
-          </button>
+          <Button
+            title="Sign In"
+            key={index}
+            handleClick={() => signIn(provider?.id)}
+          />
         ))}
       </div>
     );
