6726a1e286f1d6856017bf7af30a8349118758da
Ashe-png <ashish.khatakho11@gmail.com>
2023-07-04 08:56:29 +0545

    create  project and display project pages

---
+++ app/project/[id]/page.tsx
@@ -0,0 +1,103 @@
+import { ProjectInterface } from '@/common.tyes';
+import Modal from '@/components/Modal';
+import { getProjectDetails } from '@/lib/actions';
+import { getCurrentUser } from '@/lib/session';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Project = async ({ params: { id } }: { params: { id: string } }) => {
+  const session = await getCurrentUser();
+  const result = (await getProjectDetails(id)) as {
+    project?: ProjectInterface;
+  };
+
+  if (!result?.project) {
+    <p>Failed to fetch project information</p>;
+  }
+
+  const projectDetails = result?.project;
+  const renderLink = () => `/profile/${projectDetails?.createdBy?.id}`;
+
+  return (
+    <Modal>
+      <section className="flexBetween gap-y-8 max-w-4xl max-xs:flex-col w-full">
+        <div className="flex-1 flex items-start gap-5 w-full max-xs:flex-col">
+          <Link href={`/profile/${projectDetails?.createdBy?.id}`}>
+            <Image
+              src={projectDetails?.createdBy?.avatarUrl!}
+              width={50}
+              height={50}
+              alt="profile"
+              className="rounded-full"
+            />
+          </Link>
+          <div className="flex-1 flexStart flex-col gap-1">
+            <p className="self-start text-lg font-semibold">
+              {projectDetails?.title}
+            </p>
+            <div className="user-info">
+              <Link href={renderLink()}>{projectDetails?.createdBy?.name}</Link>
+              <Image src="/dot.svg" width={4} height={4} alt="dot" />
+              <Link
+                href={`/?category=${projectDetails?.category}`}
+                className="text-primary-purple font-semibold"
+              >
+                {projectDetails?.category}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-14">
+        <Image
+          src={`${projectDetails?.image}`}
+          className="object-cover rounded-2xl"
+          width={1064}
+          height={798}
+          alt="poster"
+        />
+      </section>
+
+      <section className="mt-20 flexCenter flex-col ">
+        <p className="max-w-5xl text-xl font-normal">
+          {projectDetails?.description}
+        </p>
+        <div className="flex flex-wrap gap-5 mt-5">
+          <Link
+            href={projectDetails?.githubUrl!}
+            target="_blank"
+            rel="noreferrer"
+            className="flexCenter gap-2 text-sm font-medium text-primary-purple"
+          >
+            🖥<span className="underline"> Github</span>
+          </Link>
+          <Image src="/dot.svg" width={4} height={4} alt="dot" />
+          <Link
+            href={projectDetails?.liveSiteUrl!}
+            target="_blank"
+            rel="noreferrer"
+            className="flexCenter gap-2 text-sm font-medium text-primary-purple"
+          >
+            🚀<span className="underline"> Live Site</span>
+          </Link>
+        </div>
+      </section>
+
+      <section className="flexCenter w-full gap-8 mt-28">
+        <span className="w-full h-0.5 bg-light-white-200" />
+        <Link href={renderLink()} className="min-w-[82px] h-[82px]">
+          <Image
+            src={projectDetails?.createdBy?.avatarUrl!}
+            alt="profile image"
+            className="rounded-full"
+            width={82}
+            height={82}
+          />
+        </Link>
+        <span className="w-full h-0.5 bg-light-white-200" />
+      </section>
+    </Modal>
+  );
+};
+export default Project;
